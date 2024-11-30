5060d42778f47e9d0cbfcaf8afdc863b22d97403
Stelios Mavris <166651850+SteliosMavris@users.noreply.github.com>
2024-11-30 03:07:23 +0200

    Allowed for user input
    Tester now takes inputs from the user for:
    -search word
    -amount of suggested words to appear

--- Tester.java
+++ Tester.java
@@ -3,36 +3,53 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Tester {
-    public static void main(String[] args) {
-        RobinHoodTrie trie = new RobinHoodTrie();
-        Scanner dictionary  = null;
-        String wordInput;
-        try {
-            dictionary = new Scanner(new File(args[0]));
-        } catch (FileNotFoundException e) {
-            System.out.println("Error: the file given as dictionary doesn't exist!");
-            System.exit(0);
-        }
-        
-        Scanner importance  = null;
-        try {
-        	importance = new Scanner(new File(args[1]));
-        } catch (FileNotFoundException e) {
-            System.out.println("Error: the file given as sample text doesn't exist!");
-            System.exit(0);
-        }
-        
-        while(dictionary.hasNextLine()) {
-        	wordInput = dictionary.nextLine();
-        	trie.insertWord(wordInput);
-        }
+	public static void main(String[] args) {
+		RobinHoodTrie trie = new RobinHoodTrie();
+		MinHeap minHeap = null;
+		Scanner dictionary = null;
+		Scanner importance = null;
+		Scanner scan = new Scanner(System.in);
+		String wordInput;
+		String searchWord;
 
-        while(importance.hasNext()) {
-        	wordInput = importance.next();
-        	if(trie.searchWord(wordInput) == true)
-        		System.out.println("Successfully detected " + wordInput);
-        }
-        System.out.println("Checking for words with prefix app:");
-        trie.autocomplete("app");
-    }
+		try {
+			dictionary = new Scanner(new File(args[0]));
+		} catch (FileNotFoundException e) {
+			System.out.println("Error: the file given as dictionary doesn't exist!");
+			System.exit(0);
+		}
+
+		try {
+			importance = new Scanner(new File(args[1]));
+		} catch (FileNotFoundException e) {
+			System.out.println("Error: the file given as sample text doesn't exist!");
+			System.exit(0);
+		}
+
+		while (dictionary.hasNextLine()) {
+			wordInput = dictionary.nextLine();
+			trie.insertWord(wordInput);
+		}
+
+		while (importance.hasNext()) {
+			wordInput = importance.next();
+			if (trie.searchWord(wordInput) == true)
+				System.out.println("Successfully detected " + wordInput);
+		}
+
+		System.out.print("Search with (one) word: ");
+		searchWord = scan.nextLine();
+
+		System.out.print("Choose (>0) amount of suggested words for " + searchWord + ": ");
+		int k = scan.nextInt();
+		while (k <= 0) {
+			System.out.println("Invalid amount given!\n Try again: ");
+			k = scan.nextInt();
+		}
+		minHeap = new MinHeap(k);
+		System.out.println("Checking for top " + k + " reccomended words for " + searchWord + ":");
+		trie.autocomplete(searchWord, minHeap);
+
+		scan.close();
+	}
 }
